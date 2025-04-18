161350ec2425617cf883b55e6027cac4a5b15ad9
bhanu77-prog-lab <bprakashm38@gmail.com>
2025-04-18 19:42:52 +0530

    settings.py

--- recipe_finder/recipe_finder/settings.py
+++ recipe_finder/recipe_finder/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipe_finder.recipes',  # Add this line
+    'recipes',  # Add this line
 ]
 
 MIDDLEWARE = [
